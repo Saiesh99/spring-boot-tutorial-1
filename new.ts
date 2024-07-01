18e7d0a38be3ed1d3db4fac505eca61e332e2664
Saiesh99 <78298468+Saiesh99@users.noreply.github.com>
2024-07-01 20:16:05 +0530

    Update new.ts

--- new.ts
+++ new.ts
@@ -3,6 +3,7 @@ import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import * as pipes from 'aws-cdk-lib/aws-eventbridge-pipes';
 
 export class EventBridgePipesStack extends cdk.Stack {
@@ -12,6 +13,7 @@ export class EventBridgePipesStack extends cdk.Stack {
     // Create a DynamoDB table
     const table = new dynamodb.Table(this, 'MyTable', {
       partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      stream: dynamodb.StreamViewType.NEW_IMAGE,
     });
 
     // Create an SQS queue
@@ -19,26 +21,32 @@ export class EventBridgePipesStack extends cdk.Stack {
 
     // Create a Lambda function
     const fn = new lambda.Function(this, 'MyFunction', {
-      runtime: lambda.Runtime.NODEJS_14_X,
+      runtime: lambda.Runtime.NODEJS_18_X,
       handler: 'index.handler',
       code: lambda.Code.fromAsset('lambda'),
     });
 
-    // Create a pipe
-    new pipes.CfnPipe(this, 'MyPipe', {
-      source: table.tableArn,
-      target: queue.queueArn,
-      enrichment: {
-        enrichmentArn: fn.functionArn,
-        inputTemplate: '{"detail-type": <DETAIL_TYPE>, "detail": <DETAIL>}',
-      },
-      filter: {
-        pattern: '{"source": ["aws.dynamodb"]}',
-      },
+    // Grant Lambda permissions to read from the SQS queue
+    queue.grantConsumeMessages(fn);
+    fn.addEventSourceMapping('SQSEventSource', {
+      eventSourceArn: queue.queueArn,
     });
 
-    // Grant necessary permissions
-    table.grantStreamRead(fn);
-    queue.grantSendMessages(fn);
+    // Create IAM role for EventBridge Pipe
+    const pipeRole = new iam.Role(this, 'PipeRole', {
+      assumedBy: new iam.ServicePrincipal('pipes.amazonaws.com'),
+    });
+    table.grantStreamRead(pipeRole);
+    queue.grantSendMessages(pipeRole);
+
+    // Create a pipe
+    new pipes.CfnPipe(this, 'MyPipe', {
+      roleArn: pipeRole.roleArn,
+      source: table.tableStreamArn!,
+      target: queue.queueArn,
+      filter: {
+        pattern: '{"eventSource": ["aws:dynamodb"]}',
+      },
+    });
   }
 }
